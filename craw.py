1cec3778fe03d8a62a5ce9f223de78c7ffc7987f
t857057 <t857057@icloud.com>
2019-04-30 06:28:39 +0800

    finisht the project

--- craw.py
+++ craw.py
@@ -3,6 +3,7 @@ import re
 import matplotlib.pyplot as plt; plt.rcdefaults()
 import numpy as np
 import matplotlib.pyplot as plt
+import time
 
 s = input("input author name: ")
 s = s.split(" ")
@@ -11,60 +12,69 @@ author = "%s"%s[0].capitalize()
 for i in range(len(s)-1):
     s[i+1] = s[i+1].replace(",","%2C")
     author = author + "+%s"%s[i+1].capitalize()
-print(author)
-# if(s[2]):
-#     author = "%s+%s+%s"%(s[0],s[1],s[2])
-# elif(s[1]):
-#     author = "%s+%s"%(s[0],s[1])
-# else:
-#     author = "%s"%s[0]
-#author = "Ian+Goodfellow"
+print("[ Author: %s ]" % author)
 
 url = "https://arxiv.org/search/?query=" + author + "&searchtype=author"
-#url = "https://arxiv.org/search/?query=Ian+Goodfellow&searchtype=all&abstracts=show&order=-announced_date_first&size=50&start=50"
 content = urllib.request.urlopen(url)
 html_str = content.read().decode('utf-8')
-pattern2 = 'has-text-black-bis has-text-weight-semibold\">Submitted[\s\S]*?<span'
+pattern1 = 'has-text-black-bis has-text-weight-semibold\">Submitted[\s\S]*?<span'
+pattern2 = "<a href=\"/search/\?searchtype=author&amp;query=[\s\S]*?</a>"
 count = {}
+count_coauthor = {}
+next_page_num = 0
 
 while(1):
+    result1 = re.findall(pattern1, html_str)
     result2 = re.findall(pattern2, html_str)
-    for r in result2:
+    for r in result1:
         date = r.split("has-text-black-bis has-text-weight-semibold\">Submitted</span>")[1].split("<span")[0].strip()
         year = date.split(" ")[2].strip(';')
         if year in count.keys():
-            print("plus")
+            #print("plus")
             count[year] += 1
         else:
             count[year] = 1
-        print(count)
+    #print(count)
+    
+    for r in result2:
+        name = r.split("\">")[1].split("</a>")[0].strip()
+        name1 = r.split("query=")[1].split("\">")[0].strip()
+        temp = name.split(' ')
+        name2 = "%s"%temp[0]
+        for i in range(len(temp)-1):
+            name2 = name2 + "+%s"%temp[i+1]
+        #print(name)
+        # print(name1)
+        # print(name2)
+        if ((author != name1) & (author != name2)):
+            if name in count_coauthor.keys():
+                count_coauthor[name] += 1
+            else:
+                count_coauthor[name] = 1
+            #print (count_coauthor)
+            #time.sleep(1)
 
-    next_url_pattern = "<nav class=\"pagination is-small is-centered breathe-horizontal\"[\s\S]*?Next"
+    next_url = []
+    next_u = ""
+    next_page_num += 50
+    next_url_pattern = "start="+str(next_page_num)+"[\s\S.]*?class=\"pagination-next\" >Next"
     next_url = re.findall(next_url_pattern, html_str)
     if(next_url):
-        print(next_url)
-        next_url = next_url[0].split("Previous")[1].split("</a>")[1].split("<a href")[1].split(">Next")[0]
-        print(next_url)
-        if(next_url[2] != '\"'): #if there exists next page
-            next_url = "https://arxiv.org/search/?query="+author+"&searchtype=author&abstracts=show&order=-announced_date_first&size=50&"+next_url.split("author&amp;")[1].split("\"")[0]
-            print(next_url)
-            next_content = urllib.request.urlopen(next_url)
-            html_str = next_content.read().decode('utf-8')
-        else:
-            break
+        next_u = "https://arxiv.org/search/?query="+author+"&searchtype=author&abstracts=show&order=-announced_date_first&size=50&start="+next_url[0].split("start=")[1].split("\"")[0]
+        #print(next_u)
+        next_content = urllib.request.urlopen(next_u)
+        html_str = next_content.read().decode('utf-8')
     else:
         break
 
+### Question 1 ###
+print(count)
 list1 = list(count.keys())
 list2 = list(count.values())
 plt.bar(list1,list2)
 plt.show()
 
-# pattern = 'title is-5 mathjax[\s\S]*?</p>'
-# result = re.findall(pattern, html_str)
-
-
-# print("[ Author: " + author + " ]")
-# for r in result:
-#     title = r.split("title is-5 mathjax\">")[1].split("</p>")[0].strip()
-#     print(title)
+### Question 2 ###
+co_list = sorted(count_coauthor)
+for i in range(len(co_list)):
+    print("[ %-29s ]: %2d times" % (co_list[i], count_coauthor[co_list[i]]))
